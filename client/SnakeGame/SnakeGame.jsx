1124ba08d71f2bd44e9c216dcc20007dbb3f6c1f
Ian Sinnott <ian@iansinnott.com>
2015-02-21 18:58:55 -0800

    Contine dev on snake board

--- client/SnakeGame/SnakeGame.jsx
+++ client/SnakeGame/SnakeGame.jsx
@@ -1,25 +1,47 @@
-var React = require('react');
+var React = require('react/addons'), // Full React + addons
+    debug = require('debug')('SnakeGame');
 
 var SnakeGame = React.createClass({
 
   getInitialState: function() {
-    return {};
+    return {
+      paused: true
+    };
+  },
+
+  // Called after component is rendered
+  componentDidMount: function() {
+    this._start();
+  },
+
+  /**
+   * Start/resume the game.
+   */
+  _start: function() {
+    debug('Game starting. Focusing board');
+    this.refs.board.getDOMNode().focus();
   },
 
   _handleClick: function(e) {
-    console.log('hey nice click');
+    debug('hey nice click');
   },
 
   _handleKey: function(e) {
-    console.log('hey nice key', e.which);
+    debug('hey nice key', e.which);
   },
 
   render: function() {
+    var classes = React.addons.classSet({
+      board: true,
+      paused: this.state.paused
+    });
+
     return (
       <div id="snake-game">
         <h2>Snake game son</h2>
         <div
-          className='board'
+          ref='board'
+          className={ classes }
           onClick={ this._handleClick }
           onKeyDown={ this._handleKey }
         />
